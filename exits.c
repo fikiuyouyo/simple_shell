aaf3f8aee26841a2e271fe515209d8a93b5c29da
Code-Gale <codewhiz001@gmail.com>
2023-05-24 19:06:05 -0700

    updated trial

---
+++ exits.c
@@ -0,0 +1,74 @@
+#include "shell.h"
+
+/**
+ **_strncpy - this copies a string
+ *@dest: is the destination string to be copied to
+ *@src: is the source string
+ *@n: is the amount of characters to be copied
+ *Return: is the concatenated string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
+}
+
+/**
+ **_strncat - this concatenates two strings
+ *@dest: indicates the first string
+ *@src: is the second string
+ *@n: indicates the amount of bytes to be maximally used
+ *Return: is the concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
+}
+
+/**
+ **_strchr - this locates a character in a string
+ *@s: indicates the string to be parsed
+ *@c: indicates the character to look for
+ *Return: (s) a pointer to the memory area s
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
+}
